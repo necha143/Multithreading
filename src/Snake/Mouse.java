ef203e95b0157998cd23d232c0703213e1b7d100
Alexander Nechaev <nechaalex143@gmail.com>
2023-06-25 22:36:35 +0500

    Added game snake

---
+++ src/Snake/Mouse.java
@@ -0,0 +1,19 @@
+package Snake;
+
+public class Mouse {
+    private int x;
+    private int y;
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/Snake/SnakeDirection.java
@@ -0,0 +1,8 @@
+package Snake;
+
+public enum SnakeDirection {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+}
